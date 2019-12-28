8faf8f0f05b39685ec728c46f14c0483dcae624b
Amol Dhage <amoltdhage@gmail.com>
2019-12-29 02:05:55 +0530

    File Added

---
+++ subtractionOfTwoNumber.py
@@ -0,0 +1,7 @@
+# This program subtract two numbers
+num1 = 6.5
+num2 = 4.5
+# Add two numbers
+sum = float(num1) - float(num2)
+# Display the Susubtraction
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
